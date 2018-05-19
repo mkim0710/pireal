d3ec23f03d20db2db5e4374f32669ed861886b96
Gabriel Acosta <acostadariogabriel@gmail.com>
2018-05-19 17:02:39 -0300

    new page

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ bundle exec jekyll build
 cd ${HTML_FOLDER}
 
 # config
-git config --global user.email "nandomoreira.me@gmail.com"
-git config --global user.name "Fernando Moreira"
+git config --global user.email "acostadariogabriel@gmail.com"
+git config --global user.name "Gabriel Acosta"
 
 # deploy
 git init
